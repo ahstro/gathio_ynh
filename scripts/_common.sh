7d1591d3b6b67efb34c55b360559b644a1e33d58
Anton Strömkvist <anton@stromkvist.com>
2019-10-09 23:54:32 +0200

    Use node the correct way

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="nodejs mongodb"
+pkg_dependencies="mongodb"
 
 #=================================================
 # PERSONAL HELPERS
